ff295c709fd7bd2f28ab95522c200e42fe93a0d9
Chin <chin.bimbo@gmail.com>
2013-08-11 18:25:48 -0400

    update le chart page

--- chart.php
+++ chart.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Test</title>
+	<title>Chart</title>
 	<script type="text/javascript" src="js/Chart.js"></script>
 
 	<link href="css/table_style.css" rel="stylesheet" />
@@ -15,13 +15,11 @@
 </head>
 <body>
 
-<?php
-	include 'topbar.php';
-?>
+	<?php include 'topbar.php';?>
 
-<div class="jumbotron">
+	<div class="jumbotron">
       
-<?php
+	<?php
 
 	include 'dbConnection.php';
 	include 'class_def.php';
@@ -88,34 +86,50 @@
 		echo "var array1=[".$cpu1->passmarkscore.",".$gpu1->passmarkscore2D.",".$gpu1->passmarkscore3D.",".$computer1->passmarkramscore.",".$computer1->passmarkdiskscore."];";
 		echo "var array2=[".$cpu2->passmarkscore.",".$gpu2->passmarkscore2D.",".$gpu2->passmarkscore3D.",".$computer2->passmarkramscore.",".$computer2->passmarkdiskscore."];";
 		echo "</script>";
-	}
+	}?>
 
-	//////////////////////////////////////
-	// Populate select boxes 
-	// (have to put this after the form submission processing code in order to set the default selected option in the select boxes)
-	// remove einherjar (id=5) from the list
-	//////////////////////////////////////	
-	echo "<form id='computerform1' method='POST'>";
+	<br>
 
-	//firat select box
-	echo "Computer 1: <select name='computerid1' id='computerid1'>";
-	foreach ($computer_array as $temp){
-		if(isset($computer1)&&$computer1->computerid==$temp->computerid&&$temp->computerid!=5) echo "<option value=".$temp->computerid." selected='selected'>".$temp->name."</option>\n";
-		else if($temp->computerid!=5) echo "<option value=".$temp->computerid.">".$temp->name."</option>\n";
-	}
-	echo "</select><br/>";
+	<form id='computerform1' method='POST' style='font-size:14px;line-height:20px;' class='form-horizontal'>
+		<div class='row'>
+			<label for='computerid1' class='col-lg-2 control-label'>Computer 1</label>
+			<div class='col-lg-2'>
+				<select name='computerid1' id='computerid1' class='form-control col-lg-3'>
+				<?php
+				//////////////////////////////////////
+				// Populate select boxes 
+				// (have to put this after the form submission processing code in order to set the default selected option in the select boxes)
+				// remove einherjar (id=5) from the list
+				//////////////////////////////////////					
 
-	//second select box
-	echo "Computer 2: <select name='computerid2' id='computerid2'>";
-	foreach ($computer_array as $temp){
-		if(isset($computer2)&&$computer2->computerid==$temp->computerid&&$temp->computerid!=5) echo "<option value=".$temp->computerid." selected='selected'>".$temp->name."</option>\n";
-		else if($temp->computerid!=5) echo "<option value=".$temp->computerid.">".$temp->name."</option>\n";
-	}
-	echo "</select><br/>";
+				//firat select box				
+				foreach ($computer_array as $temp){
+					if(isset($computer1)&&$computer1->computerid==$temp->computerid&&$temp->computerid!=5) echo "<option value=".$temp->computerid." selected='selected'>".$temp->name."</option>\n";
+					else if($temp->computerid!=5) echo "<option value=".$temp->computerid.">".$temp->name."</option>\n";
+				}
+				?>
+				</select>
+			</div>
 
-	echo "<input type='submit' name='submit' value='Submit'>";
-	echo "</form>";
+			<div class="col-lg-1"></div>
 
+			<label for='computerid2' class='col-lg-2 control-label'>Computer 2</label>
+			<div class='col-lg-2'>
+				<select name='computerid2' id='computerid2' class='form-control col-lg-3'>
+				<?php
+				//second select box
+				foreach ($computer_array as $temp){
+					if(isset($computer2)&&$computer2->computerid==$temp->computerid&&$temp->computerid!=5) echo "<option value=".$temp->computerid." selected='selected'>".$temp->name."</option>\n";
+					else if($temp->computerid!=5) echo "<option value=".$temp->computerid.">".$temp->name."</option>\n";
+				}?>
+				</select>
+			</div>
+
+			<button type='submit' name='submit' class='btn btn-default'>Go</button>
+		</div>
+	</form>
+
+	<?php
 	// Free resultset
 	pg_free_result($result);
 
@@ -123,57 +137,62 @@
 	pg_close($dbconn);
 	?>
 
-	<div style="margin-left:auto; margin-right:auto; width: 400px; float:left;">
-		<canvas id="myChart1" width="400" height="400"></canvas>
-	</div>
+		<div class="row">
+			<div class='col-lg-4'>				
+				<canvas style="float:right;" id="myChart1" width="400" height="400"></canvas>
+			</div>
 
-	<div style="margin-left:auto; margin-right:auto; width: 400px; float:left;">
-		<canvas id="myChart2" width="400" height="400"></canvas>
-	</div>
+			<div class='col-lg-1'></div>
 
-	<script>
-		//Get the context of the canvas element we want to select
-		var ctx1 = document.getElementById("myChart1").getContext("2d");
-		var ctx2 = document.getElementById("myChart2").getContext("2d");
+			<div class='col-lg-4'>				
+				<canvas style="float:right;" id="myChart2" width="400" height="400"></canvas>
+			</div>
+		</div>
 
-		var arrayMax = new Array();
 
-		for (i=0; i<5; i++){
-			arrayMax[i] = Math.max(array1[i], array2[i]);
-			array1[i] = array1[i] / arrayMax[i] * 100;
-			array2[i] = array2[i] / arrayMax[i] * 100;
-		}
+		<script>
+			//Get the context of the canvas element we want to select
+			var ctx1 = document.getElementById("myChart1").getContext("2d");
+			var ctx2 = document.getElementById("myChart2").getContext("2d");
 
-		var data1 = {
-			labels : ["CPU","2D","3D","Memory","Disk"],
-			datasets : [
-				{
-					fillColor : "rgba(220,220,220,0.5)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					data : array1
-				}
-			]
-		}
+			var arrayMax = new Array();
 
-		var data2 = {
-			labels : ["CPU","2D","3D","Memory","Disk"],
-			datasets : [
-				{
-					fillColor : "rgba(151,187,205,0.5)",
-					strokeColor : "rgba(151,187,205,1)",
-					pointColor : "rgba(151,187,205,1)",
-					pointStrokeColor : "#fff",
-					data : array2
-				}
-			]
-		}
+			for (i=0; i<5; i++){
+				arrayMax[i] = Math.max(array1[i], array2[i]);
+				array1[i] = array1[i] / arrayMax[i] * 100;
+				array2[i] = array2[i] / arrayMax[i] * 100;
+			}
 
-		var myChart1 = new Chart(ctx1).Radar(data1, {scaleOverlay:true, scaleOverride:true, scaleSteps:10, scaleStepWidth:10, scaleStartValue:0});
-		var myChart2 = new Chart(ctx2).Radar(data2, {scaleOverlay:true, scaleOverride:true, scaleSteps:10, scaleStepWidth:10, scaleStartValue:0});
-	</script>
+			var data1 = {
+				labels : ["CPU","2D","3D","Memory","Disk"],
+				datasets : [
+					{
+						fillColor : "rgba(220,220,220,0.5)",
+						strokeColor : "rgba(220,220,220,1)",
+						pointColor : "rgba(220,220,220,1)",
+						pointStrokeColor : "#fff",
+						data : array1
+					}
+				]
+			}
+
+			var data2 = {
+				labels : ["CPU","2D","3D","Memory","Disk"],
+				datasets : [
+					{
+						fillColor : "rgba(151,187,205,0.5)",
+						strokeColor : "rgba(151,187,205,1)",
+						pointColor : "rgba(151,187,205,1)",
+						pointStrokeColor : "#fff",
+						data : array2
+					}
+				]
+			}
+
+			var myChart1 = new Chart(ctx1).Radar(data1, {scaleOverlay:true, scaleOverride:true, scaleSteps:10, scaleStepWidth:10, scaleStartValue:0});
+			var myChart2 = new Chart(ctx2).Radar(data2, {scaleOverlay:true, scaleOverride:true, scaleSteps:10, scaleStepWidth:10, scaleStartValue:0});
+		</script>
 	
-</div>
+	</div>
 </body>
 </html>
