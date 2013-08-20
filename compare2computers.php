e756e8b232e2d9e49517b17cc670c7e5cfc575ac
Chin <chin.bimbo@gmail.com>
2013-08-20 15:21:50 -0400

    rename and move the chart page under computer

--- compare2computers.php
+++ compare2computers.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Chart</title>
+	<title>Compare 2 computers</title>
 	<script type="text/javascript" src="js/Chart.js"></script>
 
 	<link href="css/table_style.css" rel="stylesheet" />
--- topbar.php
+++ topbar.php
@@ -1,45 +1,43 @@
 <div class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
-  <div class="container">
-    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a href="index.php" class="navbar-brand">comdb</a>
-    <div class="nav-collapse collapse bs-navbar-collapse">
-      <ul class="nav navbar-nav">
+	<div class="container">
+		<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
 
-      	<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Computer <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<li><a href="all_computer.php">Computer list</a></li>
-				<li><a href="add_computer.php">Add new computer</a></li>
+		<a href="index.php" class="navbar-brand">comdb</a>
+
+		<div class="nav-collapse collapse bs-navbar-collapse">
+			<ul class="nav navbar-nav">
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Computer <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="all_computer.php">Computer list</a></li>
+						<li><a href="add_computer.php">Add new computer</a></li>
+						<li><a href="compare2computers.php">Compare 2 computers</a></li>
+					</ul>
+				</li>
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">CPU <b class="caret"></b></a>
+			 		<ul class="dropdown-menu">
+						<li><a href="all_cpu.php">CPU list</a></li>
+						<li><a href="add_cpu.php">Add new CPU</a></li>
+						<li><a href="chart_allcpu.php">CPU score chart</a></li>
+					</ul>
+				</li>
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">GPU <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="all_gpu.php">GPU list</a></li>
+						<li><a href="add_gpu.php">Add new GPU</a></li>
+						<li><a href="chart_allgpu.php">GPU 3D score chart</a></li>
+					</ul>
+				</li>
 			</ul>
-		</li>
-
-		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">CPU <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<li><a href="all_cpu.php">CPU list</a></li>
-				<li><a href="add_cpu.php">Add new CPU</a></li>
-        <li><a href="chart_allcpu.php">CPU score chart</a></li>
-			</ul>
-		</li>
-
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">GPU <b class="caret"></b></a>
-      <ul class="dropdown-menu">
-        <li><a href="all_gpu.php">GPU list</a></li>
-        <li><a href="add_gpu.php">Add new GPU</a></li>
-        <li><a href="chart_allgpu.php">GPU 3D score chart</a></li>
-      </ul>
-    </li>
-
-        <li>
-          <a href="chart.php">Chart</a>
-        </li>
-
-      </ul>
-    </div>
-  </div>
+		</div>
+	</div>
 </div>
