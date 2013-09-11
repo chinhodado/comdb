5e8361ec602f394743b74261a4323b8a1b0a98e9
Chin <chin.bimbo@gmail.com>
2013-09-10 20:50:44 -0400

    test branching

--- chart_allcomputer_bar.php
+++ chart_allcomputer_bar.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>All computer charts - bar</title>
+	<title>All computer charts - Bar</title>
 
 	<link href="css/table_style.css" rel="stylesheet" />
 	<link href="css/bootstrap.css" rel="stylesheet">
